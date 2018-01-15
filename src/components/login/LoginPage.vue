f1c30ab20472a6d38c2f707820160c587787e39a
margo <margo@quartsoft.com>
2018-01-15 11:09:13 +0200

    added fields for login page

--- src/components/login/LoginPage.vue
+++ src/components/login/LoginPage.vue
@@ -1,44 +1,91 @@
 <template>
-  <md-card>
-    <md-card-header>
-      <div class="md-title">
-        Login Page
-      </div>
-      <div class="md-subhead">
-        for lunch-qs
-      </div>
-    </md-card-header>
-    <md-card-content>
 
-    </md-card-content>
-  </md-card>
+  <md-content class="md-elevation-4">
+    <form class="md-layout-row md-gutter" novalidate>
+      <md-card class="md-flex-50 md-flex-small-100">
+        <md-card-header>
+          <div class="md-title">Users</div>
+        </md-card-header>
+      </md-card>
+
+      <md-card-content>
+        <div class="md-layout-row md-layout-wrap md-gutter">
+          <div class="md-flex md-flex-small-100">
+            <md-field>
+              <label for="first-name">First Name</label>
+              <md-input name="first-name" id="first-name"></md-input>
+            </md-field>
+          </div>
+
+          <div class="md-flex md-flex-small-100">
+            <md-field>
+              <label for="last-name">Last Name</label>
+              <md-input name="last-name" id="last-name"></md-input>
+            </md-field>
+          </div>
+        </div>
+
+        <div class="md-flex md-flex-small-100">
+          <md-field>
+            <label for="user-name">User Name</label>
+            <md-input name="user-name" id="user-name"></md-input>
+          </md-field>
+        </div>
+
+        <md-field>
+          <label for="email">Email</label>
+          <md-input type="email" name="email" id="email" autocomplete="email" for="email">Email</md-input>
+        </md-field>
+
+        <md-field>
+          <label for="password">Password</label>
+          <md-input type="password" name="password" id="password" for="password">Password</md-input>
+        </md-field>
+
+        <md-field>
+          <label for="confirm-password">Confirm password</label>
+          <md-input type="password" name="confirm-password" id="confirm-password" for="confirm-password">Confirm password</md-input>
+        </md-field>
+
+      </md-card-content>
+
+    </form>
+  </md-content>
 </template>
 
 <script>
-export default {
-  name: 'LoginPage',
-  data () {
-    return {
-      msg: 'Welcome to Login Page'
+  export default {
+    name: 'LoginPage',
+    data() {
+      return {
+        msg: 'Welcome to Login Page'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .md-content {
+    width: 600px;
+    margin: 0 auto;
+  }
 </style>
