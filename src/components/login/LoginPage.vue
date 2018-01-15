e88a2b04b2d9832130c83861e1e1ea51825ddc13
margo <margo@quartsoft.com>
2018-01-15 10:09:11 +0200

    initial commit

---
+++ src/components/login/LoginPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <md-card>
+    <md-card-header>
+      <div class="md-title">
+        Login Page
+      </div>
+      <div class="md-subhead">
+        for lunch-qs
+      </div>
+    </md-card-header>
+    <md-card-content>
+
+    </md-card-content>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data () {
+    return {
+      msg: 'Welcome to Login Page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
