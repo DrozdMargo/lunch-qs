a060ef0fd53dcb6a77e003c5557fca7848fa3a37
margo <margo@quartsoft.com>
2018-01-30 12:36:45 +0200

    added routes

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,4 @@
-const db = require('./db')
-const express = require('express')
-const router = express.Router()
+const db = require('../services/db')
 
 const connection = db.createConnection({
   host: '127.0.0.1',
@@ -9,21 +7,22 @@ const connection = db.createConnection({
   database: 'lunchqs'
 })
 
-router.post('/login', async (req, res) => {
+module.exports = async (req, res) => {
   const crdate = new Date()
   const users = {
+    'first_name': req.body.first_name,
+    'last_name': req.body.last_name,
     'user_name': req.body.user_name,
     'email': req.body.email,
     'password': req.body.password,
-    'last_visit': crdate
+    'created': crdate,
+    'modified': crdate
   }
   const sql = ('INSERT INTO users SET ?', users)
-  const result = await connection.queryAsync(sql)
+  const result = await connection.query(sql)
   res.send({
     'code': 200,
-    'success': 'User login sucessfully'
+    'success': 'User registered sucessfully'
   })
   console.log(result)
-})
-
-module.exports = router
+}
--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const path = require('path')
 const http = require('http')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const api = require('./routes/api')
-const login = require('./routes/login')
 
 const app = express()
 
@@ -13,34 +11,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, '../dist')))
 app.use(cors())
-app.use('/api', api)
-app.use('/login', login)
+app.use(require('./routes'))
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../dist/index.html'))
 // })
 
-const corsOption = {
-  origin: 'http://localhost:8080',
-  optionsSuccessStatus: 200
-}
-
-app.post('/api', cors(corsOption), (req, res) => {
-  console.log(req, res)
-})
-app.get('/api', cors(corsOption), (req, res) => {
-  res.send('api')
-  console.log(req.body)
-})
-app.post('/login', cors(), (req, res, next) => {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-  console.log(req.body, res)
-})
-
-// app.post('/signup', cors(), (req, res) => {
-//   res.send('signup')
-//   console.log(req, res)
-// })
 const port = process.env.PORT || '3000'
 app.set('port', port)
 
